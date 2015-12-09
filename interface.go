5f241d030cebcda2fe7f9a062f8cbce3ac7ca361
Daisuke Maki <lestrrat+github@gmail.com>
2015-12-09 09:35:24 +0900

    Add status codes

--- interface.go
+++ interface.go
@@ -17,6 +17,126 @@ type MakeXMLNoder interface {
 // TimeFormat is the format defined in xs:dateTime
 const TimeFormat = "2006-01-02T15:04:05"
 
+type StatusCode string
+
+// Error satisfies the "error" interface.
+func (s StatusCode) Error() string {
+	return string(s)
+}
+
+// Top-level status codes
+const (
+	// StatusSuccess means the request succeeded. Additional information MAY
+	// be returned in the <StatusMessage> and/or <StatusDetail> elements.
+	StatusSuccess StatusCode = "urn:oasis:names:tc:SAML:2.0:status:Success"
+
+	// ErrRequester means that the request could not be performed due to
+	// an error on the part of the requester
+	ErrRequester StatusCode = "urn:oasis:names:tc:SAML:2.0:status:Requester"
+
+	// ErrResponder means the request could not be performed due to an error
+	// on the part of the SAML responder or SAML authority.
+	ErrResponder StatusCode = "urn:oasis:names:tc:SAML:2.0:status:Responder"
+
+	// ErrVersionMismatch the SAML responder could not process the request
+	// because the version of the request message was incorrect.
+	ErrVersionMismatch StatusCode = "urn:oasis:names:tc:SAML:2.0:status:VersionMismatch"
+)
+
+// Second-level status codes described in https://www.oasis-open.org/committees/download.php/56777/sstc-saml-core-errata-2.0-wd-07-diff.pdf.
+// Quoth: "System entities are free to define more specific status codes by
+// defining appropriate URI references.
+const (
+	// ErrAuthnFailed means the responding provider was unable to 
+	// successfully authenticate the principal.
+	ErrAuthnFailed StatusCode = "urn:oasis:names:tc:SAML:2.0:status:AuthnFailed"
+
+	// ErrInvalidAttrNameOrValue means an unexpected or invalid content
+	// was encountered within a <saml:Attribute> or <saml:AttributeValue>
+	// element.
+	ErrInvalidAttrNameOrValue StatusCode = "urn:oasis:names:tc:SAML:2.0:status:InvalidAttrNameOrValue"
+
+	// ErrInvalidNameIDPolicy means the responding provider cannot or
+	// will not support the requested name identifier policy.
+	ErrInvalidNameIDPolicy StatusCode = "urn:oasis:names:tc:SAML:2.0:status:InvalidNameIDPolicy"
+
+	// ErrNoAuthnContext means the specified authentication context
+	// requirements cannot be met by the responder.
+	ErrNoAuthnContext StatusCode = "urn:oasis:names:tc:SAML:2.0:status:NoAuthnContext"
+
+	// ErrNoAvailableIDP is used by an intermediary to indicate that
+	// none of the supported identity provider <Loc> elements in an
+	// <IDPList> can be resolved or that none of the supported identity
+	// providers are available.
+	ErrNoAvailableIDP StatusCode = "urn:oasis:names:tc:SAML:2.0:status:NoAvailableIDP"
+
+	// ErrNoPassive indicates the responding provider cannot authenticate
+	// the principal passively, as has been requested.
+	ErrNoPassive StatusCode = "urn:oasis:names:tc:SAML:2.0:status:NoPassive"
+
+	// ErrNoSupportedIDP is used by an intermediary to indicate that none
+	// of the identity providers in an <IDPList> are supported by the
+	// intermediary.
+	ErrNoSupportedIDP StatusCode = "urn:oasis:names:tc:SAML:2.0:status:NoSupportedIDP"
+
+	// ErrPartialLogout is used by a session authority to indicate to
+	// a session participant that it was not able to propagate logout
+	// to all other session participants.
+	ErrPartialLogout StatusCode = "urn:oasis:names:tc:SAML:2.0:status:PartialLogout"
+
+	// ErrProxyCountExceeded indicates that a responding provider cannot
+	// authenticate the principal directly and is not permitted to proxy
+	// the request further.
+	ErrProxyCountExceeded StatusCode = "urn:oasis:names:tc:SAML:2.0:status:ProxyCountExceeded"
+
+	// ErrRequestDenied means the SAML responder or SAML authority is
+	// able to process the request but has chosen not to respond. This
+	// status code MAY be used when there is concern about the security
+	// context of the request message or the sequence of request messages
+	// received from a particular requester.
+	ErrRequestDenied StatusCode = "urn:oasis:names:tc:SAML:2.0:status:RequestDenied"
+
+	// ErrRequestUnsupported means the SAML responder or SAML authority
+	// does not support the request.
+	ErrRequestUnsupported StatusCode = "urn:oasis:names:tc:SAML:2.0:status:RequestUnsupported"
+
+	// ErrRequestVersionDeprecated means the SAML responder cannot process
+	// any requests with the protocol version specified in the request.
+	ErrRequestVersionDeprecated StatusCode = "urn:oasis:names:tc:SAML:2.0:status:RequestVersionDeprecated"
+
+	// ErrRequestVersionTooHigh means the SAML responder cannot process
+	// the request because the protocol version specified in the request
+	// message is a major upgrade from the highest protocol version supported
+	// by the responder.
+	ErrRequestVersionTooHigh StatusCode = "urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooHigh"
+
+	// ErrRequestVersionTooLow means the SAML responder cannot process
+	// the request because the protocol version specified in the request
+	// message is too low.
+	ErrRequestVersionTooLow StatusCode = "urn:oasis:names:tc:SAML:2.0:status:RequestVersionTooLow"
+
+	// ErrResourceNotRecognized means the resource value provided in the
+	// request message is invalid or unrecognized.
+	ErrResourceNotRecognized StatusCode = "urn:oasis:names:tc:SAML:2.0:status:ResourceNotRecognized"
+
+	// ErrTooManyResponses means the response message would contain more
+	// elements than the SAML responder is able to return.
+	ErrTooManyResponses StatusCode = "urn:oasis:names:tc:SAML:2.0:status:TooManyResponses"
+
+	// ErrUnknownAttrProfile means an entity that has no knowledge of a
+	// particular attribute profile has been presented with an attribute
+	// drawn from that profile.
+	ErrUnknownAttrProfile StatusCode = "urn:oasis:names:tc:SAML:2.0:status:UnknownAttrProfile"
+
+	// ErrUnknownPrincipal means the responding provider does not recognize
+	// the principal specified or implied by the request.
+	ErrUnknownPrincipal StatusCode = "urn:oasis:names:tc:SAML:2.0:status:UnknownPrincipal"
+
+	// ErrUnsupportedBinding means the SAML responder cannot properly fulfill
+	// the request using the protocol binding specified in the request.
+	ErrUnsupportedBinding StatusCode = "urn:oasis:names:tc:SAML:2.0:status:UnsupportedBinding"
+)
+
 type AuthenticationMethod string
 type ConfirmationMethod string
 
